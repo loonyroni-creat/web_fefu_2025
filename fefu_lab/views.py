097e6e11068a65ad172d8eb919d53359834afec1
loonyroni-creat <alaayaundertaker@gmail.com>
2025-10-18 22:17:12 +1000

    Добавлено представление профиля студента с обработкой 404

--- fefu_lab/views.py
+++ fefu_lab/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import Http404
 
 def home_page(request):
     return HttpResponse("Добро пожаловать на главную страницу!")
@@ -7,4 +8,8 @@ def home_page(request):
 def about_page(request):
     return HttpResponse("Страница 'О нас'")
 
+def student_profile(request, student_id):
+    if student_id > 100:
+        raise Http404("Студент не найден")
+    return HttpResponse(f"Профиль студента с ID: {student_id}")
 # Create your views here.
