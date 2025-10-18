0d10651c91f4c130366b16c8db1a1ba50711e4a3
loonyroni-creat <alaayaundertaker@gmail.com>
2025-10-18 22:15:00 +1000

    Добавлены базовые представления для главной и о нас

--- fefu_lab/views.py
+++ fefu_lab/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def home_page(request):
+    return HttpResponse("Добро пожаловать на главную страницу!")
+
+def about_page(request):
+    return HttpResponse("Страница 'О нас'")
 
 # Create your views here.
