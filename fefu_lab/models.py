a1f74ea69369762b0234525022c33cec41db77fc
loonyroni-creat <alaayaundertaker@gmail.com>
2025-10-26 22:04:38 +1000

    Создана модель UserProfile для хранения пользователей

--- fefu_lab/models.py
+++ fefu_lab/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+class UserProfile(models.Model):
+    username = models.CharField(max_length=50, unique=True, verbose_name='Логин')
+    email = models.EmailField(unique=True, verbose_name='Email')
+    password = models.CharField(max_length=128, verbose_name='Пароль')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата регистрации')
+
+    def __str__(self):
+        return self.username
+
 # Create your models here.
