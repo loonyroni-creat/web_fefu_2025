ca32209f0a1256682e834f9a4875a03214a813da
loonyroni-creat <alaayaundertaker@gmail.com>
2025-10-26 22:17:04 +1000

    Добавлены новые маршруты для форм обратной связи и регистрации

--- fefu_lab/urls.py
+++ fefu_lab/urls.py
@@ -2,8 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # СТАРЫЕ МАРШРУТЫ
     path('', views.home_page, name='home'),
     path('about/', views.about_page, name='about'),
     path('student/<int:student_id>/', views.student_profile, name='student_profile'),
     path('course/<slug:course_slug>/', views.CourseDetailView.as_view(), name='course_detail'),
+    
+    # НОВЫЕ МАРШРУТЫ ДЛЯ ФОРМ
+    path('feedback/', views.feedback_view, name='feedback'),
+    path('register/', views.register_view, name='register'),
 ]
