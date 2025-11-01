b674dfe240495252b798357a6810c276c287a7ce
loonyroni-creat <alaayaundertaker@gmail.com>
2025-11-01 12:18:08 +1000

    Добавлена модель Instructor для преподавателей

--- fefu_lab/models.py
+++ fefu_lab/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.urls import reverse
+from django.core.exceptions import ValidationError
 
 class UserProfile(models.Model):
     username = models.CharField(max_length=50, unique=True, verbose_name='Логин')
@@ -8,5 +10,46 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.username
+class Instructor(models.Model):
+    first_name = models.CharField(
+        max_length=100,
+        verbose_name='Имя'
+    )
+    last_name = models.CharField(
+        max_length=100,
+        verbose_name='Фамилия'
+    )
+    email = models.EmailField(
+        unique=True,
+        verbose_name='Email'
+    )
+    specialization = models.CharField(
+        max_length=200,
+        verbose_name='Специализация'
+    )
+    degree = models.CharField(
+        max_length=100,
+        blank=True,
+        verbose_name='Ученая степень'
+    )
+    is_active = models.BooleanField(
+        default=True,
+        verbose_name='Активен'
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата создания'
+    )
 
+    class Meta:
+        verbose_name = 'Преподаватель'
+        verbose_name_plural = 'Преподаватели'
+        ordering = ['last_name', 'first_name']
+
+    def __str__(self):
+        return f"{self.last_name} {self.first_name}"
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
 # Create your models here.
