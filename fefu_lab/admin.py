e0d076fd3030127488bfc9554f68734b771cad35
loonyroni-creat <alaayaundertaker@gmail.com>
2025-11-01 12:43:46 +1000

    Настроен Django Admin с поиском, фильтрацией и предзаполнением полей

--- fefu_lab/admin.py
+++ fefu_lab/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['last_name', 'first_name', 'email', 'faculty', 'is_active']
+    list_filter = ['is_active', 'faculty']
+    search_fields = ['first_name', 'last_name', 'email']
+    list_per_page = 20
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['title', 'instructor', 'level', 'duration', 'price', 'is_active']
+    list_filter = ['is_active', 'level', 'instructor']
+    search_fields = ['title', 'description']
+    prepopulated_fields = {'slug': ['title']}
 # Register your models here.
