6b78dd0437f5f028f648d80b764425576df2ae46
loonyroni-creat <alaayaundertaker@gmail.com>
2025-11-28 19:35:15 +1000

    Созданы формы регистрации, входа и редактирования профиля

--- fefu_lab/forms.py
+++ fefu_lab/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 from django.core.exceptions import ValidationError
 from .models import UserProfile
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .models import Student
 
 class FeedbackForm(forms.Form):
     """Форма обратной связи"""
@@ -72,3 +75,113 @@ class RegistrationForm(forms.Form):
             raise ValidationError("Пароли не совпадают")
         
         return cleaned_data
+class UserRegistrationForm(UserCreationForm):
+    """
+    Форма регистрации пользователя с дополнительными полями
+    """
+    email = forms.EmailField(
+        required=True,
+        label='Email',
+        widget=forms.EmailInput(attrs={'class': 'form-control'})
+    )
+    first_name = forms.CharField(
+        required=True,
+        label='Имя',
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
+    last_name = forms.CharField(
+        required=True,
+        label='Фамилия',
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
+    faculty = forms.ChoiceField(
+        choices=Student.FACULTY_CHOICES,
+        label='Факультет',
+        widget=forms.Select(attrs={'class': 'form-control'})
+    )
+    phone = forms.CharField(
+        required=False,
+        label='Телефон',
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
+    bio = forms.CharField(
+        required=False,
+        label='О себе',
+        widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 4})
+    )
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+        }
+
+    def clean_email(self):
+        """
+        Проверка уникальности email
+        """
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise ValidationError('Пользователь с таким email уже существует')
+        return email
+
+    def save(self, commit=True):
+        """
+        Сохранение пользователя и создание профиля
+        """
+        user = super().save(commit=False)
+        user.email = self.cleaned_data['email']
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        
+        if commit:
+            user.save()
+            # Обновляем профиль студента
+            profile = user.student_profile
+            profile.faculty = self.cleaned_data['faculty']
+            profile.phone = self.cleaned_data['phone']
+            profile.bio = self.cleaned_data['bio']
+            profile.save()
+        
+        return user
+
+class UserLoginForm(forms.Form):
+    """
+    Форма входа пользователя
+    """
+    username = forms.CharField(
+        label='Email или имя пользователя',
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
+    password = forms.CharField(
+        label='Пароль',
+        widget=forms.PasswordInput(attrs={'class': 'form-control'})
+    )
+
+class UserProfileForm(forms.ModelForm):
+    """
+    Форма редактирования профиля пользователя
+    """
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email']
+        widgets = {
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+        }
+
+class StudentProfileForm(forms.ModelForm):
+    """
+    Форма редактирования профиля студента
+    """
+    class Meta:
+        model = Student
+        fields = ['faculty', 'phone', 'bio', 'birth_date', 'avatar']
+        widgets = {
+            'faculty': forms.Select(attrs={'class': 'form-control'}),
+            'phone': forms.TextInput(attrs={'class': 'form-control'}),
+            'bio': forms.Textarea(attrs={'class': 'form-control', 'rows': 4}),
+            'birth_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+        }
