4f67ed4665dd06eec5c3c4b046f0965ee1b7e0c9
loonyroni-creat <alaayaundertaker@gmail.com>
2026-02-02 19:02:26 +0500

    Исправлены ошибки 3

--- web_2025/settings.py
+++ web_2025/settings.py
@@ -1,5 +1,8 @@
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -83,10 +86,11 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
+
 
 
 # ======================
