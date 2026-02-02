13b7fca93a3a6e4ff20dd2228008f6747a50604a
loonyroni-creat <alaayaundertaker@gmail.com>
2026-02-02 15:23:13 +0500

    Исправлены ошибки в settings.py

--- web_2025/settings.py
+++ web_2025/settings.py
@@ -61,6 +61,21 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'web_2025.urls'
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
 WSGI_APPLICATION = 'web_2025.wsgi.application'
 
 
